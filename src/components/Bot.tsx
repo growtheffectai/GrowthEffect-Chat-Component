4613fc264092df2ad9eff0e1aec2293d4e47d2d9
canob <canobuyuk96@gmail.com>
2025-07-31 13:45:47 +0300

    starting message changed to Turkish

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -183,7 +183,7 @@ export type LeadsConfig = {
   successMessage?: string;
 };
 
-const defaultWelcomeMessage = 'Hi there! How can I help?';
+const defaultWelcomeMessage = 'Merhaba, size nasıl yardımcı olabilirim?';
 
 /*const sourceDocuments = [
     {
