40747a44d7bb012204bdb29767bdd260de8358f9
BegumYyaprak <bgmyslyprk@gmail.com>
2025-09-07 10:22:38 +0300

    Flowise geçen yerlerin GE ile değiştirilmesi

--- src/components/buttons/SendButton.tsx
+++ src/components/buttons/SendButton.tsx
@@ -28,8 +28,8 @@ export const SendButton = (props: SendButtonProps) => {
   );
 };
 export const DeleteButton = (props: SendButtonProps) => {
-  // Check if <flowise-fullchatbot> is present in the DOM
-  const isFullChatbot = document.querySelector('flowise-fullchatbot') !== null;
+  // Check if <growtheffect-fullchatbot> is present in the DOM
+  const isFullChatbot = document.querySelector('growtheffect-fullchatbot') !== null;
   const paddingClass = isFullChatbot ? 'px-4' : 'px-12';
 
   return (
